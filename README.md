a163477c88ff2eb157ea07611a874455c8b484bc
eia485 <eia485git@gmail.com>
2023-06-19 23:15:24 -0700

    Add project files.

---
+++ README.md
@@ -0,0 +1,8 @@
+# SlotlessInspectorOpener
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that makes it so pressing the open inspector button on a refeditor targeting a worker that has no slot will open it's parent worker.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [SlotlessInspectorOpener.dll](https://github.com/eia485/NeosSlotlessInspectorOpener/releases/latest/download/SlotlessInspectorOpener.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
---
+++ SlotlessInspectorOpener/SlotlessInspectorOpener.cs
@@ -0,0 +1,33 @@
+﻿using NeosModLoader;
+using HarmonyLib;
+using FrooxEngine;
+
+namespace SlotlessInspectorOpener
+{
+    public class SlotlessInspectorOpener : NeosMod
+    {
+        public override string Name => "SlotlessInspectorOpener";
+        public override string Author => "eia485";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/EIA485/NeosSlotlessInspectorOpener";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.eia485.SlotlessInspectorOpener");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(InspectorHelper), nameof(InspectorHelper.OpenInspectorForTarget))]
+        class SlotlessInspectorOpenerPatch
+        {
+            static void Prefix(ref IWorldElement target, ref bool openWorkerOnly)
+            {
+                if (!openWorkerOnly && target.FindNearestParent<Slot>() == null && target.FindNearestParent<User>() == null)
+                {
+                    openWorkerOnly = true;
+                    if (target.Parent?.FindNearestParent<Worker>() != null)
+                        target = target.Parent;
+                }
+            }
+        }
+    }
+}
